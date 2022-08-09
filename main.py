09c6388da7590d568bc579ae0623f7378b7c31d6
Monayam Reshad <reshadmrrr@gmail.com>
2022-08-10 04:47:28 +0600

    login, last 5 tweet showcasing done

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+from getpass import getpass
+import traceback
+
+
+from util import TweeterUser
+
 
 def main():
-    pass
+    print("Please login to your account")
+    username = input("Username: ")
+    password = getpass()
+    print("Logging in ...")
+    try:
+        user = TweeterUser(username=username, password=password)
+        if hasattr(user, "access_token") and len(user.access_token) > 0:
+            print("Login successful ...")
+        else:
+            print("Login failed ...")
+        print("Checking recent tweets ...")
+        user.get_last_5_tweets()
+    except:
+        print(traceback.format_exc())
+        print("Login failed ...")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,6 +1,12 @@
 import json
+import traceback
 from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 """
 TODO: Write whatever class or function you may need
@@ -9,24 +15,40 @@ feel free to make any changes in the given class and its methods' arguments or i
 """
 
 
+BASE_URL = os.getenv("API")
+
+
 class NetworkRequest:
     @staticmethod
-    def get(url, headers = {}):
-        req = Request(url=url, method='GET')
-        for key, value in headers.items():
-            req.add_header(key, value)
-        
-        result = {}
-        with urlopen(req) as res:
-            body = res.read().decode('utf-8')
-            result['body'] = json.loads(body)
-            result['code'] = res.status
-        
-        return result
-    
+    def _request(req):
+        req.add_header("Content-Type", "application/json")
+        res = {}
+        try:
+            with urlopen(req) as r:
+                # print(r)
+                body = r.read().decode("utf-8")
+                res["body"] = json.loads(body)
+                # res["code"] = r.status
+        except:
+            print(traceback.format_exc())
+        return res["body"]
+
     @staticmethod
-    def post():
-        pass
+    def get(endpoint="", headers={}):
+        return NetworkRequest._request(
+            Request(url=BASE_URL + endpoint, method="GET", headers=headers)
+        )
+
+    @staticmethod
+    def post(endpoint="", headers={}, body={}):
+        return NetworkRequest._request(
+            Request(
+                url=BASE_URL + endpoint,
+                headers=headers,
+                data=bytes(json.dumps(body), encoding="utf-8"),
+                method="POST",
+            )
+        )
 
     @staticmethod
     def put():
@@ -36,3 +58,25 @@ class NetworkRequest:
     def delete():
         pass
 
+
+class TweeterUser:
+    def __init__(self, username, password) -> None:
+        res = NetworkRequest.post(
+            endpoint="auth", body={"username": username, "password": password}
+        )
+        # print(res)
+        self.access_token = res["access_token"]
+        self.refresh_token = res["refresh_token"]
+        self.token_type = res["token_type"]
+
+    def get_last_5_tweets(self):
+        res = NetworkRequest.get(
+            endpoint="tweets",
+            headers={
+                "Authorization": f"{self.token_type.capitalize()} {self.access_token}"
+            },
+        )
+        for r in res:
+            print(
+                f"({r['id']})  {r['author']['username']} at {r['created_at']}\n{r['text']}\n"
+            )
